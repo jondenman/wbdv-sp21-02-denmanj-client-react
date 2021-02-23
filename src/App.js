88b68ac3d9db87330c1d3b99803dab3553845ce5
denmanj <denman.j@northeastern.edu>
2021-02-23 15:49:37 -0500

    added home landing page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 import CourseManager from "./components/course-manager";
 import {BrowserRouter} from "react-router-dom";
+import {Route} from 'react-router-dom';
+import Home from "./components/home"
 
 function App() {
   return (
       <BrowserRouter>
-        <div className="container-fluid">
-          <CourseManager/>
-        </div>
+          <div className="container-fluid">
+              <Route path="/" exact={true}>
+                  <Home/>
+              </Route>
+              <Route path="/courses">
+                  <CourseManager/>
+              </Route>
+          </div>
       </BrowserRouter>
+      // <BrowserRouter>
+      //   <div className="container-fluid">
+      //     <CourseManager/>
+      //   </div>
+      // </BrowserRouter>
   );
 }
 

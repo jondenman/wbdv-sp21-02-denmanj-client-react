862e9caaea08ba181b6f7504a3213d542b87351e
denmanj <denman.j@northeastern.edu>
2021-03-07 15:03:07 -0500

    implemented lesson-service.js

---
+++ src/services/lesson-service.js
@@ -0,0 +1,41 @@
+const MODULES_URL = "https://wbdv-generic-server.herokuapp.com/api/991350206/modules"
+const LESSONS_URL = "https://wbdv-generic-server.herokuapp.com/api/991350206/lessons"
+
+export const createLesson = (moduleId, lesson) =>
+    fetch(`${MODULES_URL}/${moduleId}/lessons`, {
+        method: "POST",
+        body: JSON.stringify(lesson),
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+
+export const updateLesson = (lessonId, lesson) =>
+    fetch(`${LESSONS_URL}/${lessonId}`, {
+        method: "PUT",
+        body: JSON.stringify(lesson),
+        headers: {
+            'content-type': 'application/json'
+        }
+
+    })
+        .then(response => response.json())
+
+export const findLessonsForModule = (moduleId) =>
+    fetch(`${MODULES_URL}/${moduleId}/lessons`)
+        .then(response => response.json())
+
+export const deleteLesson = (lessonId) =>
+    fetch(`${LESSONS_URL}/${lessonId}`, {
+        method: "DELETE"
+    })
+
+const api = {
+    createLesson,
+    findLessonsForModule,
+    deleteLesson,
+    updateLesson
+}
+
+export default api
